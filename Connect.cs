de52ca745515b870cf7070304e73a0386b4cbe2d
lapma12 <lapostyanm@kkszki.hu>
2025-09-29 10:25:46 +0200

    Connect.cs class is ready to use.

--- Connect.cs
+++ Connect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace ComputerShop
 {
     internal class Connect
     {
-    }
+        public MySqlConnection Connection;
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _pass;
+
+        private string _connectionString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "computershop";
+            _user = "root";
+            _pass = "";
+            _connectionString = $"Server={_host};Database={_db};User ID={_user};Password={_pass};SslMode=None";
+
+            Connection = new MySqlConnection(_connectionString);
+
+            try
+            {
+                Connection.Open();
+                Console.WriteLine("Sikeres csatlakozás az adatbázishoz.");
+                Connection.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hiba történt " + ex.Message);
+            }
+        }
+    }    
 }
